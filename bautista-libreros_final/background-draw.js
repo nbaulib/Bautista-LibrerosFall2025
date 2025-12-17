16a90cc52054ed6ba1b7c95021bf322b8c416de2
nbaulib <69161557+nbaulib@users.noreply.github.com>
2025-12-16 19:41:49 -0500

    bruise when click

--- bautista-libreros_final/background-draw.js
+++ bautista-libreros_final/background-draw.js
@@ -6,17 +6,7 @@
 // This is considered best practice.
 window.addEventListener('load', function () {
     resize();
-    // document.addEventListener('mouseenter', startPainting);
-    // document.addEventListener('mouseleave', stopPainting);
-
-    document.addEventListener('mousemove', startPainting);
-    document.addEventListener('mouseout', stopPainting);
-
-    // document.addEventListener('mousedown', stopPainting);
-    // document.addEventListener('mouseup', stopPainting);
-
-
-    document.addEventListener('mousemove', sketch);
+    document.addEventListener('mousedown', drawDots);
     window.addEventListener('resize', resize);
 });
 
@@ -31,57 +21,22 @@ function resize() {
     ctx.canvas.height = window.innerHeight;
 }
 
-// Stores the initial position of the cursor
-let coord = { x: 0, y: 0 };
-
-// This is the flag that we are going to use to 
-// trigger drawing
-let paint = false;
-
 // Updates the coordianates of the cursor when 
 // an event e is triggered to the coordinates where 
 // the said event is triggered.
 function getPosition(event) {
-    coord.x = event.clientX - canvas.offsetLeft;
-    coord.y = event.clientY - canvas.offsetTop;
+    return {
+        x: event.clientX - canvas.offsetLeft,
+        y: event.clientY - canvas.offsetTop
+    };
 }
 
-// The following functions toggle the flag to start
-// and stop drawing
-function startPainting(event) {
-    paint = true;
-    getPosition(event);
-}
-function stopPainting() {
-    paint = false;
-}
+function drawDots(event) {
+    const coord = getPosition(event);
 
-function sketch(event) {
-    if (!paint) return;
     ctx.beginPath();
-
-    ctx.lineWidth = 10;
-
-    // Sets the end of the lines drawn
-    // to a round shape.
-    ctx.lineCap = 'round';
-
-    //  NEW brown color
-    ctx.strokeStyle = '#56310c40';
-
-    // The cursor to start drawing
-    // moves to this coordinate
-    ctx.moveTo(coord.x, coord.y);
-
-    // The position of the cursor
-    // gets updated as we move the
-    // mouse around.
-    getPosition(event);
-
-    // A line is traced from start
-    // coordinate to this coordinate
-    ctx.lineTo(coord.x, coord.y);
-
-    // Draws the line.
-    ctx.stroke();
+    ctx.arc(coord.x, coord.y, 5, 0, Math.PI * 2);
+    // ctx.arc(coord.x+20, coord.y+5, 10, 20, Math.PI * 2);
+    ctx.fillStyle = '#56310cac';
+    ctx.fill();
 }
